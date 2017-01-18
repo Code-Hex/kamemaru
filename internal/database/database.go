65ce2a78ff0e917c02e96a3ccd916f9841c9269e
K <x00.x7f@gmail.com>
2017-01-18 11:22:52 +0900

    Modified database package

--- internal/database/database.go
+++ internal/database/database.go
@@ -2,18 +2,28 @@ package database
 
 import "github.com/jinzhu/gorm"
 
+var (
+	UserTable  = new(User)
+	ImageTable = new(Image)
+)
+
 type User struct {
 	gorm.Model
-	Name    string `gorm:"not null"`
-	Pass    string `gorm:"not null"`
+	Name    string `gorm:"type:varchar(255);not null"`
+	Pass    string `gorm:"type:varchar(64);not null"`
+	Salt    string `gorm:"type:varchar(64);not null"`
 	IconURL string
 	Images  []Image
 }
 
 type Image struct {
 	gorm.Model
-	Name        string `gorm:"not null"`
+	Name        string `gorm:"type:varchar(255);not null"`
 	Ext         string `gorm:"type:varchar(8);not null"`
-	Hash        string `gorm:"not null"`
-	OriginalURL string `gorm:"not null"`
+	Hash        string `gorm:"type:varchar(255);not null"`
+	OriginalURL string `gorm:"type:varchar(255);not null"`
+}
+
+func IsExistUser(db *gorm.DB, username string) bool {
+	return !db.Where("name = ?", username).First(UserTable).RecordNotFound()
 }
